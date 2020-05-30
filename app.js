0a153c2e4c6fcb1dfa9622e57da5a3cdb0ffab06
primeos1 <akhenisio@yahoo.com>
2020-05-30 01:49:23 +0100

    fixed keys file

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.static(`${__dirname}/public`));
 app.get('/', (req,res) =>{
     res.render('index',  {
       stripePublishableKey: keys.stripePublishableKey
+      
     });
 })
 
